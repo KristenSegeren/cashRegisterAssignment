b776221f88c40de4fc9460cca0f38f7cf5d60e60
KristenSegeren <krissege449@ed.amdsb.ca>
2023-02-24 13:50:18 -0500

    Add project files.

---
+++ cashRegisterAssignment/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace cashRegisterAssignment
+{
+    partial class CounterfeitMarks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.levisLabel = new System.Windows.Forms.Label();
+            this.wranglerLabel = new System.Windows.Forms.Label();
+            this.silversLabel = new System.Windows.Forms.Label();
+            this.levisInput = new System.Windows.Forms.TextBox();
+            this.wranglersInput = new System.Windows.Forms.TextBox();
+            this.silversInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.subLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.subTotalOutput = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.taxTotalOutput = new System.Windows.Forms.Label();
+            this.lineLabels = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tenderAmountLabel = new System.Windows.Forms.Label();
+            this.tenderInput = new System.Windows.Forms.TextBox();
+            this.calculateChangeButton = new System.Windows.Forms.Button();
+            this.printReciptButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.changeOutput = new System.Windows.Forms.Label();
+            this.reciptLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.SteelBlue;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-17, -3);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(822, 64);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "The Denim Shop (Counterfeit Marks)";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 1;
+            // 
+            // levisLabel
+            // 
+            this.levisLabel.AutoSize = true;
+            this.levisLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.levisLabel.Location = new System.Drawing.Point(1, 74);
+            this.levisLabel.Name = "levisLabel";
+            this.levisLabel.Size = new System.Drawing.Size(132, 20);
+            this.levisLabel.TabIndex = 2;
+            this.levisLabel.Text = "Number of Levis";
+            // 
+            // wranglerLabel
+            // 
+            this.wranglerLabel.AutoSize = true;
+            this.wranglerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wranglerLabel.Location = new System.Drawing.Point(1, 155);
+            this.wranglerLabel.Name = "wranglerLabel";
+            this.wranglerLabel.Size = new System.Drawing.Size(169, 20);
+            this.wranglerLabel.TabIndex = 3;
+            this.wranglerLabel.Text = "Number of Wranglers";
+            // 
+            // silversLabel
+            // 
+            this.silversLabel.AutoSize = true;
+            this.silversLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.silversLabel.Location = new System.Drawing.Point(1, 111);
+            this.silversLabel.Name = "silversLabel";
+            this.silversLabel.Size = new System.Drawing.Size(143, 20);
+            this.silversLabel.TabIndex = 4;
+            this.silversLabel.Text = "Number of Silvers";
+            // 
+            // levisInput
+            // 
+            this.levisInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.levisInput.Location = new System.Drawing.Point(198, 68);
+            this.levisInput.Name = "levisInput";
+            this.levisInput.Size = new System.Drawing.Size(100, 26);
+            this.levisInput.TabIndex = 5;
+            // 
+            // wranglersInput
+            // 
+            this.wranglersInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.wranglersInput.Location = new System.Drawing.Point(198, 155);
+            this.wranglersInput.Name = "wranglersInput";
+            this.wranglersInput.Size = new System.Drawing.Size(100, 26);
+            this.wranglersInput.TabIndex = 6;
+            // 
+            // silversInput
+            // 
+            this.silversInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.silversInput.Location = new System.Drawing.Point(198, 111);
+            this.silversInput.Name = "silversInput";
+            this.silversInput.Size = new System.Drawing.Size(100, 26);
+            this.silversInput.TabIndex = 7;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(4, 187);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(307, 27);
+            this.calculateButton.TabIndex = 8;
+            this.calculateButton.Text = "Calculate Totals";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // subLabel
+            // 
+            this.subLabel.AutoSize = true;
+            this.subLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subLabel.Location = new System.Drawing.Point(23, 231);
+            this.subLabel.Name = "subLabel";
+            this.subLabel.Size = new System.Drawing.Size(71, 18);
+            this.subLabel.TabIndex = 9;
+            this.subLabel.Text = "Sub Total";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(23, 294);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(69, 18);
+            this.totalLabel.TabIndex = 10;
+            this.totalLabel.Text = "       Total";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(23, 263);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(69, 18);
+            this.taxLabel.TabIndex = 11;
+            this.taxLabel.Text = "Tax Total";
+            // 
+            // subTotalOutput
+            // 
+            this.subTotalOutput.BackColor = System.Drawing.Color.White;
+            this.subTotalOutput.Location = new System.Drawing.Point(198, 228);
+            this.subTotalOutput.Name = "subTotalOutput";
+            this.subTotalOutput.Size = new System.Drawing.Size(100, 21);
+            this.subTotalOutput.TabIndex = 12;
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.BackColor = System.Drawing.Color.White;
+            this.totalOutput.Location = new System.Drawing.Point(198, 291);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(100, 21);
+            this.totalOutput.TabIndex = 13;
+            // 
+            // taxTotalOutput
+            // 
+            this.taxTotalOutput.BackColor = System.Drawing.Color.White;
+            this.taxTotalOutput.Location = new System.Drawing.Point(198, 260);
+            this.taxTotalOutput.Name = "taxTotalOutput";
+            this.taxTotalOutput.Size = new System.Drawing.Size(100, 21);
+            this.taxTotalOutput.TabIndex = 14;
+            // 
+            // lineLabels
+            // 
+            this.lineLabels.BackColor = System.Drawing.Color.Black;
+            this.lineLabels.Location = new System.Drawing.Point(-11, 321);
+            this.lineLabels.Name = "lineLabels";
+            this.lineLabels.Size = new System.Drawing.Size(322, 10);
+            this.lineLabels.TabIndex = 15;
+            this.lineLabels.Text = "label5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 16;
+            // 
+            // tenderAmountLabel
+            // 
+            this.tenderAmountLabel.AutoSize = true;
+            this.tenderAmountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderAmountLabel.Location = new System.Drawing.Point(15, 346);
+            this.tenderAmountLabel.Name = "tenderAmountLabel";
+            this.tenderAmountLabel.Size = new System.Drawing.Size(79, 20);
+            this.tenderAmountLabel.TabIndex = 17;
+            this.tenderAmountLabel.Text = "Tendered";
+            // 
+            // tenderInput
+            // 
+            this.tenderInput.Location = new System.Drawing.Point(198, 344);
+            this.tenderInput.Name = "tenderInput";
+            this.tenderInput.Size = new System.Drawing.Size(100, 22);
+            this.tenderInput.TabIndex = 18;
+            // 
+            // calculateChangeButton
+            // 
+            this.calculateChangeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateChangeButton.Location = new System.Drawing.Point(3, 374);
+            this.calculateChangeButton.Name = "calculateChangeButton";
+            this.calculateChangeButton.Size = new System.Drawing.Size(308, 28);
+            this.calculateChangeButton.TabIndex = 19;
+            this.calculateChangeButton.Text = "Calculate Change";
+            this.calculateChangeButton.UseVisualStyleBackColor = true;
+            this.calculateChangeButton.Click += new System.EventHandler(this.calculateChangeButton_Click);
+            // 
+            // printReciptButton
+            // 
+            this.printReciptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printReciptButton.Location = new System.Drawing.Point(5, 461);
+            this.printReciptButton.Name = "printReciptButton";
+            this.printReciptButton.Size = new System.Drawing.Size(306, 26);
+            this.printReciptButton.TabIndex = 20;
+            this.printReciptButton.Text = "Print Recipt ";
+            this.printReciptButton.UseVisualStyleBackColor = true;
+            this.printReciptButton.Click += new System.EventHandler(this.printReciptButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 420);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 20);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Change Amount";
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.BackColor = System.Drawing.Color.White;
+            this.changeOutput.Location = new System.Drawing.Point(195, 419);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(100, 21);
+            this.changeOutput.TabIndex = 22;
+            // 
+            // reciptLabel
+            // 
+            this.reciptLabel.BackColor = System.Drawing.Color.SteelBlue;
+            this.reciptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reciptLabel.ForeColor = System.Drawing.Color.White;
+            this.reciptLabel.Location = new System.Drawing.Point(374, 68);
+            this.reciptLabel.Name = "reciptLabel";
+            this.reciptLabel.Size = new System.Drawing.Size(400, 422);
+            this.reciptLabel.TabIndex = 23;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(3, 499);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(785, 34);
+            this.resetButton.TabIndex = 24;
+            this.resetButton.Text = "NEW ORDER";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // CounterfeitMarks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 534);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.reciptLabel);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.printReciptButton);
+            this.Controls.Add(this.calculateChangeButton);
+            this.Controls.Add(this.tenderInput);
+            this.Controls.Add(this.tenderAmountLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lineLabels);
+            this.Controls.Add(this.taxTotalOutput);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.subTotalOutput);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.subLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.silversInput);
+            this.Controls.Add(this.wranglersInput);
+            this.Controls.Add(this.levisInput);
+            this.Controls.Add(this.silversLabel);
+            this.Controls.Add(this.wranglerLabel);
+            this.Controls.Add(this.levisLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "CounterfeitMarks";
+            this.Text = "Counterfeit Marks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label levisLabel;
+        private System.Windows.Forms.Label wranglerLabel;
+        private System.Windows.Forms.Label silversLabel;
+        private System.Windows.Forms.TextBox levisInput;
+        private System.Windows.Forms.TextBox wranglersInput;
+        private System.Windows.Forms.TextBox silversInput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label subLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label subTotalOutput;
+        private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.Label taxTotalOutput;
+        private System.Windows.Forms.Label lineLabels;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label tenderAmountLabel;
+        private System.Windows.Forms.TextBox tenderInput;
+        private System.Windows.Forms.Button calculateChangeButton;
+        private System.Windows.Forms.Button printReciptButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label changeOutput;
+        private System.Windows.Forms.Label reciptLabel;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ cashRegisterAssignment/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cashRegisterAssignment
+{
+    public partial class CounterfeitMarks : Form
+    {
+        public CounterfeitMarks()
+        {
+            InitializeComponent();
+        }
+        //Variables set up
+        int levis = 95;
+        int wrangler = 70;
+        int silver = 103;
+        double numOfLevis, numOfWrangler, numOfSilver, subTotal, taxTotal, total, tender, change;
+
+
+
+        double finalLevis;
+        double finalWranglers;
+        double finalSilver;
+
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //input
+            numOfLevis = Convert.ToDouble(levisInput.Text);
+            numOfWrangler = Convert.ToDouble(wranglersInput.Text);
+            numOfSilver = Convert.ToDouble(silversInput.Text);
+
+            //calculations
+            finalLevis = levis * numOfLevis;
+            finalWranglers = wrangler * numOfWrangler;
+            finalSilver = silver * numOfSilver;
+
+            subTotal = finalLevis + finalWranglers + finalSilver;
+            taxTotal = subTotal * 0.13;
+            total = subTotal + taxTotal;
+
+            //output
+            subTotalOutput.Text = $"{subTotal.ToString("C")}";
+            taxTotalOutput.Text = $"{taxTotal.ToString("C")}";
+            totalOutput.Text = $"{total.ToString("C")}";
+        }
+        private void calculateChangeButton_Click(object sender, EventArgs e)
+        {
+            tender = Convert.ToDouble(tenderInput.Text);
+            change = tender - total;
+            changeOutput.Text = $"{change.ToString("C")}";
+
+        }
+        private void printReciptButton_Click(object sender, EventArgs e)
+        {
+            reciptLabel.Text = $"The Denim Shop\n\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"You bought...\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"{numOfLevis} Levis                        @${levis}\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"{numOfSilver} Silvers                      @${silver}\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"{numOfWrangler} Wranglers                @${wrangler}\n\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"for a total of...\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"${total}\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"you paid...\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"${tender} \n\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"You will recieve {change.ToString("C")} in change\n\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"Thank You For Shopping at Counterfeit Marks\n";
+            Thread.Sleep(800);
+            Refresh();
+            reciptLabel.Text += $"Have A Wonderful Day:)\n";
+            Thread.Sleep(800);
+            Refresh();
+        }
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            reciptLabel.Text = "";
+            subTotalOutput.Text = "";
+            taxTotalOutput.Text = "";
+            totalOutput.Text = "";
+            changeOutput.Text = "";
+            tenderInput.Text = "";
+            levisInput.Text = "";
+            wranglersInput.Text = "";
+            silversInput.Text = "";
+        }
+    }
+}
